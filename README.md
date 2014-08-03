5f2fa4c581016c89a8224aa76059fefb461e0638
Peter Brown <peter@venturebeat.com>
2014-08-03 14:54:37 -0700

    Initial Onedaycard

---
+++ README.md
@@ -0,0 +1,4 @@
+onedaycard
+==========
+
+Send a 24 hour e-card someone you know.
---
+++ application/views/template_body.php
@@ -0,0 +1,6 @@
+<form action="publish" method="POST">
+	To: <input name="to" type="text"> <br>
+	From: <input name="from" type="text"><br>
+	Message: <textarea name="message" rows="3" cols="10"></textarea>
+	<input type="submit" value="submit">
+</form>
